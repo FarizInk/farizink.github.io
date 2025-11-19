faefc2e066c53525767d1a0f84698ca773cb6b55
FarizInk <nizaralfariziakbar10@gmail.com>
2025-11-19 21:48:36 +0700

    chore: remove build

--- src/main.js
+++
@@ -1,9 +0,0 @@
-import './app.css';
-import App from './App.svelte';
-import { mount } from 'svelte';
-const target = document.getElementById('app');
-if (!target) {
-    throw new Error('Could not find app element');
-}
-mount(App, { target });
-//# sourceMappingURL=main.js.map
