cb0d7191d395e051fb689c18a263923115c9ed33
fariz <nizaralfariziakbar10@gmail.com>
2026-03-16 14:00:36 +0700

    chore: change to ssh push

--- gh-pages.cjs
+++ gh-pages.cjs
@@ -4,7 +4,7 @@ ghpages.publish(
   'dist', // path to deploy directory
   {
     branch: 'gh-pages',
-    repo: 'https://github.com/FarizInk/farizink.github.io.git', // Update to point to your repository
+    repo: 'git@github.com:FarizInk/farizink.github.io.git', // Update to point to your repository
     user: {
       name: 'Fariz', // update to use your name
       email: 'nizaralfariziakbar10@gmail.com' // Update to use your email
